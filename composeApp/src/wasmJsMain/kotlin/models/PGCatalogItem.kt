7de5b3bd3fb208a24681088f903aa2b4955b4d71
ROBERTO ANTONIO GUTIERREZ CAMPOS <roberto.gutierrez@elektra.com.mx>
2024-06-14 15:08:08 -0600

    correccion en consumo

--- composeApp/src/wasmJsMain/kotlin/models/PGCatalogItem.kt
+++ composeApp/src/wasmJsMain/kotlin/models/PGCatalogItem.kt
@@ -4,23 +4,24 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class PGCatalog(
-    val productList: ArrayList<PGCatalogItem>,
+    val items: ArrayList<PGCatalogItem>? = null,
+    val amount: Int? = 0,
 )
 @Serializable
 data class PGCatalogItem(
-    val category: String,
-    val code: String,
-    val description: String,
-    val hasOffer: Boolean,
-    val id: String,
-    val image: String,
-    val isActive: Boolean,
-    val price_discount: Double,
-    val price_discount_label: String,
-    val price_label: String,
-    val price_normal: Double,
-    val price_normal_label: String,
-    val store: String,
-    val title: String,
-    val url: String
+    val category: String? = "",
+    val code: String? = "",
+    val description: String? = "",
+    val hasOffer: Boolean? = null,
+    val id: String? = "",
+    val image: String? = "",
+    val isActive: Boolean? = null,
+    val price_discount: Double? = null,
+    val price_discount_label: String? = "",
+    val price_label: String? = "",
+    val price_normal: Double? = null,
+    val price_normal_label: String? = "",
+    val store: String? = "",
+    val title: String? = "",
+    val url: String? = "",
 )
